54be5700985ef09f902bd1d073aa64f4cad82e71
unknown <986520756@qq.com>
2020-06-28 15:51:50 +0800

    完成资料管理部分提交

--- wisdomConstruction/pages/dataManagement/index.js
+++ wisdomConstruction/pages/dataManagement/index.js
@@ -1,6 +1,241 @@
 ;(function(global) {
+    // todo 模拟数据，后期走接口
+    const dataSource = {
+        education: [
+            {
+                "updateMan": "bowei01",
+                "fileName": "木工3.6.9三级教育2-4月",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 10:33:39",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267282066417602561",
+                "createMan": "bowei01",
+                "fileSize": 34466751,
+                "createTime": "2020-06-01 10:33:39",
+                "id": "1267283134585212930",
+                "fileInfoId": "1267283044290236417",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            },
+            {
+                "updateMan": "bowei01",
+                "fileName": "木工2.5.8三级教育2-4月",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 12:30:57",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267282066417602561",
+                "createMan": "bowei01",
+                "fileSize": 41913696,
+                "createTime": "2020-06-01 12:30:45",
+                "id": "1267312602884288513",
+                "fileInfoId": "1267312555731922945",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            },
+            {
+                "updateMan": "bowei01",
+                "fileName": "木工1.4三级教育2-4月",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 12:35:15",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267282066417602561",
+                "createMan": "bowei01",
+                "fileSize": 26481390,
+                "createTime": "2020-06-01 12:35:15",
+                "id": "1267313737288286209",
+                "fileInfoId": "1267312951930073090",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            },
+            {
+                "updateMan": "bowei01",
+                "fileName": "泥工三级教育2-4月",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 12:38:19",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267282066417602561",
+                "createMan": "bowei01",
+                "fileSize": 51609952,
+                "createTime": "2020-06-01 12:38:19",
+                "id": "1267314509493207042",
+                "fileInfoId": "1267314301355110401",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            },
+            {
+                "updateMan": "bowei01",
+                "fileName": "管理人员三级教育",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 12:39:41",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267282066417602561",
+                "createMan": "bowei01",
+                "fileSize": 15924666,
+                "createTime": "2020-06-01 12:39:41",
+                "id": "1267314854038548482",
+                "fileInfoId": "1267314775135301633",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            },
+            {
+                "updateMan": "bowei01",
+                "fileName": "钢筋工三级教育2-4月1",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 13:43:42",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267282066417602561",
+                "createMan": "bowei01",
+                "fileSize": 29875299,
+                "createTime": "2020-06-01 13:43:21",
+                "id": "1267330876355280897",
+                "fileInfoId": "1267330792351805441",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            },
+            {
+                "updateMan": "bowei01",
+                "fileName": "钢筋工三级教育2-4月2",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 13:44:52",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267282066417602561",
+                "createMan": "bowei01",
+                "fileSize": 31528783,
+                "createTime": "2020-06-01 13:44:52",
+                "id": "1267331255881117697",
+                "fileInfoId": "1267331222959980545",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            },
+            {
+                "updateMan": "bowei01",
+                "fileName": "架子工三级教育",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 15:36:51",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267282066417602561",
+                "createMan": "bowei01",
+                "fileSize": 35375154,
+                "createTime": "2020-06-01 15:36:51",
+                "id": "1267359436860194818",
+                "fileInfoId": "1267359383206658050",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            }
+        ],
+        data: [
+            {
+                createMan: "bowei01",
+                createTime: "2020-06-01 15:58:38",
+                fileCategoryId: "1267364316446404610",
+                fileFormat: ".pdf",
+                fileInfoId: "1267364890822782977",
+                fileName: "4月晨会",
+                fileSize: 20469289,
+                id: "1267364921491488770",
+                isDelete: 0,
+                ownerId: "hfjsh",
+                projectId: "1263755638233243649",
+                updateMan: "bowei01",
+                updateTime: "2020-06-01 15:58:38"
+            },
+        ],
+        train: [
+            {
+                "updateMan": "bowei01",
+                "fileName": "安全教育培训",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 15:59:51",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267364386256400385",
+                "createMan": "bowei01",
+                "fileSize": 16985618.00,
+                "createTime": "2020-06-01 15:59:51",
+                "id": "1267365224525758466",
+                "fileInfoId": "1267365206251220993",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            },
+        ],
+        disclose: [
+            {
+                "updateMan": "bowei01",
+                "fileName": "塔吊、司索交底",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 16:07:49",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267364495211835394",
+                "createMan": "bowei01",
+                "fileSize": 4776111.00,
+                "createTime": "2020-06-01 16:07:49",
+                "id": "1267367228929159169",
+                "fileInfoId": "1267365891000664065",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            }, {
+                "updateMan": "bowei01",
+                "fileName": "脚手架交底",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 16:11:49",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267364495211835394",
+                "createMan": "bowei01",
+                "fileSize": 14296073.00,
+                "createTime": "2020-06-01 16:11:49",
+                "id": "1267368239177564162",
+                "fileInfoId": "1267367386119090177",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            }, {
+                "updateMan": "bowei01",
+                "fileName": "主体部分交底、机械类交底",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 16:13:50",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267364495211835394",
+                "createMan": "bowei01",
+                "fileSize": 14015493.00,
+                "createTime": "2020-06-01 16:13:50",
+                "id": "1267368745522331650",
+                "fileInfoId": "1267368648705257473",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            }, {
+                "updateMan": "bowei01",
+                "fileName": "交底签名",
+                "isDelete": 0,
+                "updateTime": "2020-06-01 16:14:11",
+                "ownerId": "hfjsh",
+                "fileCategoryId": "1267364495211835394",
+                "createMan": "bowei01",
+                "fileSize": 1257230.00,
+                "createTime": "2020-06-01 16:14:11",
+                "id": "1267368831472009217",
+                "fileInfoId": "1267368817676988418",
+                "projectId": "1263755638233243649",
+                "fileFormat": ".pdf"
+            }
+        ]
+    };
     const tree = layui.tree;
     const table = layui.table;
+     // 右侧表格渲染
+     const filesTable = table.render({
+        elem: '#files-table',
+        skin: 'nob',
+        even: true,
+        cols: [
+            [
+                {field: 'fileName', title: '文件名称', width: '45%', align: 'center'},
+                {field: 'fileFormat', title: '文件格式', width:'15%', align: 'center'},
+                {field: 'fileSize', title: '文件大小', width:'15%', align: 'center', templet: (columns) => { return `${ (columns.fileSize / 1024  / 1024).toFixed(2)}M`}},
+                {field: 'updateTime', title: '上传日期', width: '15%', align: 'center'},
+                {field: 'operate', title: '操作', width: '10%', align: 'center', templet: (columns) => { return `<a href="http://zlsite.app-saas.com//hf/base/c/filemanage/downloadFile?attachCode=${columns.fileInfoId}">下载</a>`} },
+            ]
+        ],
+        data: dataSource.disclose,
+
+    });
     // 左侧树形结构
     const filesTree = tree.render({
         elem: '#files',
@@ -11,25 +246,38 @@
                 children: [
                     {
                         title: '三级教育',
-                        field: 'education',
+                        id: 'education',
                     },
                     {
                         title: '晨会资料',
-                        field: 'data',
+                        id: 'data',
                     },
                     {
                         title: '安全培训',
-                        field: 'train',
+                        id: 'train',
                     },
                     {
                         title: '安全交底',
-                        field: 'disclose',
+                        id: 'disclose',
                     },
                 ]
             }
         ],
         click(obj) {
-            console.log(obj)
+            const { elem, data: { id } } = obj;
+            $(elem).parent().children().removeClass('active');
+            $(elem).addClass('active');
+            filesTable.reload({
+                data: dataSource[id],
+            });
         },
-    })
+    });
+    // 默认选中最后一条数据
+    $(files).find('div.layui-tree-set').each((index, val) => {
+        if ($(val).data('id') === 'disclose') {
+            $(val).addClass('active');
+            return;
+        }
+    });
+   
 })(window)
